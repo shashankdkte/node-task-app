bc158a660050d941ab8362214456565e6b762c7e
Shashank Dhakate <shashankdkte@gmail.com>
2022-12-05 00:34:37 +0530

    authentication completed with pagination

--- src/models/user.js
+++ src/models/user.js
@@ -2,52 +2,66 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
+const Task = require("./task");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 7,
-    trim: true,
-    validate(value) {
-      if (value.includes("password")) {
-        throw new Error("Password should not contain these value");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be positive");
-      }
-    },
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true,
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
       },
     },
-  ],
+    password: {
+      type: String,
+      required: true,
+      minlength: 7,
+      trim: true,
+      validate(value) {
+        if (value.includes("password")) {
+          throw new Error("Password should not contain these value");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      default: 0,
+      validate(value) {
+        if (value < 0) {
+          throw new Error("Age must be positive");
+        }
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//Adding Reference to Task
+
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
 });
 //Hiding private data from user
 
@@ -95,6 +109,15 @@ userSchema.pre("save", async function (next) {
   if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
   }
+  next();
+});
+
+//Delete user tasks when user is removed
+
+userSchema.pre("remove", async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
+  console.log(user._id);
   next();
 });
 const User = mongoose.model("User", userSchema);
--- src/routers/user.js
+++ src/routers/user.js
@@ -12,7 +12,7 @@ router.post("/users", async (req, res) => {
   try {
     await user.save();
     const token = await user.generateAuthToken();
-    res.send({ user, token });
+    res.status(201).send({ user, token });
   } catch (ex) {
     res.status(400).send(ex);
     console.log(ex);
@@ -22,6 +22,7 @@ router.post("/users", async (req, res) => {
 router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
+
 router.post("/users/login", async (req, res) => {
   try {
     const user = await User.findByCredentials(
@@ -59,6 +60,7 @@ router.post("/users/logoutAll", auth, async (req, res) => {
     res.status(500).send();
   }
 });
+
 router.get("/users", auth, async (req, res) => {
   try {
     const users = await User.find({});
@@ -78,6 +80,30 @@ router.get("/users/:id", async (req, res) => {
     res.send(user);
   } catch (error) {
     res.status(500).send(error);
+  }
+});
+
+router.patch("/users/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidation) {
+    return res.status(400).send({ error: "Invalid Update" });
+  }
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    // const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    //   new: true,
+    //   runValidators: true,
+    // });
+
+    res.send(req.user);
+  } catch (error) {
+    res.status(404).send(e);
   }
 });
 
@@ -106,6 +132,15 @@ router.patch("/users/:id", async (req, res) => {
     res.send(user);
   } catch (error) {
     res.status(404).send(e);
+  }
+});
+
+router.delete("/users/me", auth, async (req, res) => {
+  try {
+    await req.user.remove();
+    res.send(req.user);
+  } catch (error) {
+    res.status(500).send();
   }
 });
 
